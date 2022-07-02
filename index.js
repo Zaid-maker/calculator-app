90c1c88a21e953be115c44d83978ede43c430cb4
Zaid-maker <pzhafeez@gmail.com>
2022-07-02 14:20:52 +0500

    fixed

--- index.js
+++ index.js
@@ -12,12 +12,9 @@ function inputDigit(digit) {
     calculator.displayValue = digit;
     calculator.waitingForSecondOperand = false;
   } else {
-    // Overwrite `displayValue` if the current value is '0' otherwise append to it
     calculator.displayValue =
       displayValue === "0" ? digit : displayValue + digit;
   }
-
-  console.log(calculator);
 }
 
 function inputDecimal(dot) {
@@ -27,25 +24,21 @@ function inputDecimal(dot) {
     return;
   }
 
-  // If the `displayValue` does not contain a decimal point
   if (!calculator.displayValue.includes(dot)) {
-    // Append the decimal point
     calculator.displayValue += dot;
   }
 }
 
 function handleOperator(nextOperator) {
   const { firstOperand, displayValue, operator } = calculator;
-
   const inputValue = parseFloat(displayValue);
 
   if (operator && calculator.waitingForSecondOperand) {
     calculator.operator = nextOperator;
-    console.log(calculator);
     return;
   }
 
-  if (firstOperand === null && !isNaN(inputValue)) {
+  if (firstOperand == null && !isNaN(inputValue)) {
     calculator.firstOperand = inputValue;
   } else if (operator) {
     const result = calculate(firstOperand, inputValue, operator);
@@ -56,8 +49,6 @@ function handleOperator(nextOperator) {
 
   calculator.waitingForSecondOperand = true;
   calculator.operator = nextOperator;
-
-  console.log(calculator);
 }
 
 function calculate(firstOperand, secondOperand, operator) {
@@ -79,7 +70,6 @@ function resetCalculator() {
   calculator.firstOperand = null;
   calculator.waitingForSecondOperand = false;
   calculator.operator = null;
-  console.log(calculator);
 }
 
 function updateDisplay() {
@@ -92,28 +82,30 @@ updateDisplay();
 const keys = document.querySelector(".calculator-keys");
 keys.addEventListener("click", (event) => {
   const { target } = event;
+  const { value } = target;
   if (!target.matches("button")) {
     return;
   }
 
-  if (target.classList.contains("operator")) {
-    handleOperator(target.value);
-    updateDisplay();
-    return;
+  switch (value) {
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+    case "=":
+      handleOperator(value);
+      break;
+    case ".":
+      inputDecimal(value);
+      break;
+    case "all-clear":
+      resetCalculator();
+      break;
+    default:
+      if (Number.isInteger(parseFloat(value))) {
+        inputDigit(value);
+      }
   }
 
-  if (target.classList.contains("decimal")) {
-    inputDecimal(target.value);
-    updateDisplay();
-    return;
-  }
-
-  if (target.classList.contains("all-clear")) {
-    resetCalculator();
-    updateDisplay();
-    return;
-  }
-
-  inputDigit(target.value);
   updateDisplay();
 });
